906cd5ddb59a1e316821f0cc2fe10924f260fa0b
Jeremy <Jeremy>
2017-04-09 14:44:10 -0400

    Added the BasicRoom.cs which inherits from BasicEntity, and adds a description field.

---
+++ TextGame/BasicRoom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextGame
+{
+	public class BasicRoom : BasicEntity
+	{
+		public string Description;
+
+		public BasicRoom()
+		{
+			Description = string.Empty;
+		}
+	}
+}
