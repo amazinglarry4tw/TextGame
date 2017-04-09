fd8f1b9142ce18d17d84fe043a1792054694be97
Jeremy <Jeremy>
2017-04-09 14:23:39 -0400

    This is the intial commit for the new Text Based game.

---
+++ TextGame/BasicEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextGame
+{
+	public class BasicEntity
+	{
+		public string Name;
+		public Guid Id;
+
+		public BasicEntity()
+		{
+			Name = string.Empty;
+			Id = Guid.NewGuid();
+		}
+	}
+}
