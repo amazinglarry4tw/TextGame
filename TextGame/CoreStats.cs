dd6bd9551b31377d20073a1968cf634dd37f4079
Jeremy <Jeremy>
2017-04-22 14:15:13 -0400

    Added the CoreStats which will be the stats for all player & non player characters (eventually).  Added the property to the BasicPlayerCharacter.

---
+++ TextGame/CoreStats.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextGame
+{
+    public class CoreStats
+    {
+        public int Strength;
+        public int Constitution;
+
+        public CoreStats()
+        {
+            Strength = 50;
+            Constitution = 50;
+        }
+    }
+}
