44b26ebd324521c9ae87acaca7611dd7d2491e2a
Jeremy <Jeremy>
2017-04-22 21:15:03 -0400

    Check in all the things.

--- TextGame/Program.cs
+++ TextGame/Program.cs
@@ -8,16 +8,23 @@ namespace TextGame
 {
 	class Program
 	{
+        enum ActionType { MOVEMENT = 1, SPEECH, ATTACK, ROLEPLAY };
         static PlayerCharacter pc = new PlayerCharacter();
-        static BasicRoom rm = new BasicRoom();
+        static RoomManager rm = new RoomManager();
+        static string input = string.Empty;
 
         static void Main(string[] args)
 		{
             Setup();
 
-            Console.WriteLine(string.Format("{0} has {1} Constitution and {2} Health.", pc.Name, pc.Stats.Constitution, pc.Health));
-            Console.WriteLine(string.Format("{0}\n{1}", rm.Name, rm.Description));
-            Console.ReadLine();
+            //Console.WriteLine(string.Format("{0} has {1} Constitution and {2} Health.", pc.Name, pc.Stats.Constitution, pc.Health));
+            Console.WriteLine(string.Format("{0}\n{1}", rm.GameRooms[0].Name, rm.GameRooms[0].Description));
+
+            while (input != "exit")
+            {
+                input = Console.ReadLine();
+                HandleInput(input);
+            }
 		}
 
         static void Setup()
@@ -26,7 +33,45 @@ namespace TextGame
             pc.GenerateInfo();
             pc.Name = "Jeremy";
 
-            rm.SetRoomInfo("Town Square Central", "This room is as crazy as it is big.  It's wild.");
+            BasicRoom room1 = new BasicRoom();
+            room1.SetRoomInfo("Town Square Central", "This is the main room in the game.  It's where merchants and players will gather.");
+            rm.AddRoom(room1);
+
+            BasicRoom room2 = new BasicRoom();
+            room2.SetRoomInfo("Staging Area", "The staging area is where players will gather to spell up before they go out into the wilds.");
+            rm.AddRoom(room2);
+
+            rm.ConnectRoom(room1.Id, "west", room2.Id);
+            rm.ConnectRoom(room2.Id, "east", room1.Id);
+        }
+
+        static void HandleInput(string input)
+        {
+            if (input != "exit")
+            {
+                int type = EvaluateType(input);
+
+                switch (type)
+                {
+                    case (int)ActionType.SPEECH:
+                        Console.WriteLine("The player is trying to speak.");
+                        break;
+                    default:
+                        Console.WriteLine("Not sure what the player is doing.  It has not been captured");
+                        break;
+                }
+            }
+        }
+
+        static int EvaluateType(string input)
+        {
+            input.Trim();
+            int type = 0;
+
+            if (input[0].Equals('\'') || input[0].Equals('"'))
+                return (int)ActionType.SPEECH;
+            else
+                return 0;
         }
 	}
 }
