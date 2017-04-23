42adc193513ab1d40974c8fdb938693de7d40bba
Jeremy <Jeremy>
2017-04-23 10:43:38 -0400

    Moved the Input parser and handler into its own class.

--- TextGame/Program.cs
+++ TextGame/Program.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TextGame
 {
 	class Program
 	{
-        enum ActionType { MOVEMENT = 1, SPEECH, ATTACK, ROLEPLAY };
+        static InputManager IM = new InputManager();
         static PlayerCharacter pc = new PlayerCharacter();
         static RoomManager rm = new RoomManager();
-        static string input = string.Empty;
+        static string Input = string.Empty;
 
         static void Main(string[] args)
 		{
@@ -21,10 +20,10 @@ namespace TextGame
             //Console.WriteLine(string.Format("{0} has {1} Constitution and {2} Health.", pc.Name, pc.Stats.Constitution, pc.Health));
             Console.WriteLine(string.Format("{0}\n{1}", rm.GameRooms[0].Name, rm.GameRooms[0].Description));
 
-            while (input != "exit")
+            while (Input != "exit")
             {
-                input = Console.ReadLine();
-                HandleInput(input);
+                Input = Console.ReadLine();
+                IM.HandleInput(Input);
             }
 		}
 
@@ -44,50 +43,6 @@ namespace TextGame
 
             rm.ConnectRoom(room1.Id, "west", room2.Id);
             rm.ConnectRoom(room2.Id, "east", room1.Id);
-        }
-
-        static void HandleInput(string input)
-        {
-            if (input != "exit")
-            {
-                int type = EvaluateType(input);
-
-                switch (type)
-                {
-                    case (int)ActionType.SPEECH:
-                        Console.WriteLine("The player is trying to speak.");
-                        break;
-                    case (int)ActionType.MOVEMENT:
-                        Console.WriteLine("The player is trying to move.");
-                        break;
-                    default:
-                        Console.WriteLine("Not sure what the player is doing.  It has not been captured");
-                        break;
-                }
-            }
-        }
-
-        static int EvaluateType(string input)
-        {
-            string temp = input.Trim();
-            int type = 0;
-            string[] parsedInput = ParseInput(temp);
-
-            if (input[0].Equals('\'') || input[0].Equals('"') || parsedInput[0] == "say")
-                type = (int)ActionType.SPEECH;
-            else if (parsedInput[0].Equals("go") || parsedInput[0].Equals("move"))
-                type = (int)ActionType.MOVEMENT;
-            else
-                type = 0;
-
-            return type;
-        }
-
-        static string[] ParseInput(string input)
-        {
-            string temp = Regex.Replace(input, @"\s+", " ");
-            string[] words = temp.Split(' ');
-            return words;
         }
 	}
 }
